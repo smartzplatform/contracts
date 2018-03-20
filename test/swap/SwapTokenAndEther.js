7eca1d9235126ea6cecdd25814bacf58be3092c5
quantum13 <quant13@gmail.com>
2018-03-20 19:54:27 +0300

    swap tokens and ether

---
+++ test/swap/SwapTokenAndEther.js
@@ -0,0 +1,137 @@
+'use strict';
+
+import expectThrow from '../helpers/expectThrow';
+
+
+const Token = artifacts.require("TokenForSwap.sol");
+const Swap = artifacts.require("SwapTokenAndEther.sol");
+const l = console.log;
+
+const finney = x => web3.toWei(x, 'finney');
+const balanceOf =  async addr => web3.eth.getBalance(addr);
+
+contract('SwapTokenAndEther', function(accounts) {
+
+    const role =  {
+        tokenOwner: accounts[0],
+		participant1: accounts[1],
+		participant2: accounts[2],
+        nobody: accounts[9]
+	};
+
+    let instance, token;
+
+    beforeEach(async function() {
+        token = await Token.new({from: role.tokenOwner});
+        await token.mint(role.participant1, 100, {from:role.tokenOwner});
+
+        //participant 1 want to swap
+        instance = await Swap.new(
+            role.participant1,
+            token.address,
+            50,
+            role.participant2,
+            finney(20)
+        );
+    });
+
+    it("complex test", async function() {
+
+
+        assert.equal(0, await token.balanceOf(role.participant2));
+
+        await expectThrow(instance.swap({from: role.nobody}));
+
+        await expectThrow(instance.swap({from: role.participant1}));
+        await expectThrow(instance.swap({from: role.participant2}));
+
+        // only p2 can send ether
+        await expectThrow(instance.sendTransaction({from: role.participant1, value: finney(19)}));
+
+        // not full amount
+        await token.transfer(instance.address, 49, {from: role.participant1});
+        await expectThrow(instance.swap({from: role.nobody}));
+        await expectThrow(instance.swap({from: role.participant1}));
+        await expectThrow(instance.swap({from: role.participant2}));
+
+        // not full amount
+        await instance.sendTransaction({from: role.participant2, value: finney(19)});
+        await expectThrow(instance.swap({from: role.nobody}));
+        await expectThrow(instance.swap({from: role.participant1}));
+        await expectThrow(instance.swap({from: role.participant2}));
+
+        // not full amount
+        await token.transfer(instance.address, 1, {from: role.participant1});
+        await expectThrow(instance.swap({from: role.nobody}));
+        await expectThrow(instance.swap({from: role.participant1}));
+        await expectThrow(instance.swap({from: role.participant2}));
+
+        //success
+        let part1InitialBalance = await balanceOf(role.participant1);
+        await instance.sendTransaction({from: role.participant2, value: finney(1)});
+        await expectThrow(instance.swap({from: role.nobody}));
+        await instance.swap({from: role.participant1});
+
+        assert.equal(50, await token.balanceOf(role.participant2));
+        assert(part1InitialBalance.add(finney(20)) - await balanceOf(role.participant1) < 100000);
+
+        //no second swap
+        await expectThrow(instance.swap({from: role.participant1}));
+        await expectThrow(instance.swap({from: role.participant2}));
+
+        //no second sending of ether
+        await expectThrow(instance.sendTransaction({from: role.participant2, value: finney(19)}));
+    });
+
+    it("swap more than expected", async function() {
+
+        assert.equal(0, await token.balanceOf(role.participant2));
+
+
+        await token.transfer(instance.address, 51, {from: role.participant1});
+        await instance.sendTransaction({from: role.participant2, value: finney(21)});
+
+        let part1InitialBalance = await balanceOf(role.participant1);
+        await instance.swap({from: role.participant1});
+
+        assert.equal(51, await token.balanceOf(role.participant2));
+        assert(part1InitialBalance.add(finney(21)) - await balanceOf(role.participant1) < 100000);
+    });
+
+    it("refund", async function() {
+
+        // p1
+        assert.equal(100, await token.balanceOf(role.participant1));
+        await token.transfer(instance.address, 51, {from: role.participant1});
+        assert.equal(49, await token.balanceOf(role.participant1));
+        await expectThrow(instance.refund({from: role.nobody}));
+        await instance.refund({from: role.participant1});
+        assert.equal(100, await token.balanceOf(role.participant1));
+
+        // p2
+        let part1InitialBalance = await balanceOf(role.participant1);
+        await instance.sendTransaction({from: role.participant2, value: finney(21)});
+        await expectThrow(instance.refund({from: role.nobody}));
+        await instance.refund({from: role.participant2});
+        assert(part1InitialBalance - await balanceOf(role.participant1) < 100000);
+    });
+
+    it("refund after swap", async function() {
+
+        await token.transfer(instance.address, 51, {from: role.participant1});
+        await instance.sendTransaction({from: role.participant2, value: finney(21)});
+        await instance.swap({from: role.participant2});
+
+        // p1
+        assert.equal(49, await token.balanceOf(role.participant1));
+        await token.transfer(instance.address, 44, {from: role.participant1});
+        assert.equal(5, await token.balanceOf(role.participant1));
+        await expectThrow(instance.refund({from: role.nobody}));
+        await instance.refund({from: role.participant1});
+        assert.equal(49, await token.balanceOf(role.participant1));
+
+        // p2
+        await expectThrow(instance.sendTransaction({from: role.participant2, value: finney(19)}));
+    });
+
+});
--- truffle.js
+++ truffle.js
@@ -2,27 +2,28 @@ require('babel-register');
 require('babel-polyfill');
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-  networks: {
-    development: {
-      host: "localhost",
-      port: 9545,
-      network_id: "*" // Match any network id
-    },
+    // See <http://truffleframework.com/docs/advanced/configuration>
+    // to customize your Truffle configuration!
+    networks: {
+        development: {
+            host: "localhost",
+            port: 9545,
+            gasPrice: 1,
+            network_id: "*" // Match any network id
+        },
 
-    ropsten: {  // testnet
-      host: "localhost",
-      port: 8547,
-      network_id: 3
+        ropsten: {  // testnet
+            host: "localhost",
+            port: 8547,
+            network_id: 3
+        }
+    },
+    solc: {
+        optimizer: {
+            enabled: true,
+            runs: 200
+        }
     }
-  },
-  solc: {
-    optimizer: {
-      enabled: true,
-      runs: 200
-    }
-  }
 
 
 };
